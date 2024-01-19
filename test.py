3c908e629cdf595ab4180766e56cef941578c16d
FailedFeather37 <126341708+FailedFeather37@users.noreply.github.com>
2024-01-19 08:52:38 +0100

    Add files via upload

---
+++ test.py
@@ -0,0 +1,11 @@
+﻿def aléatoire():
+     x1=0
+     x2=0
+     l=[]
+     for i in range(100):
+        x = random.uniform(0,1)
+        y = random.uniform(0,1)
+        l.append(x)
+        l.append(y)
+     return l
+print(aléatoire())
