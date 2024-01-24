f1d2d94be0e46b9e5f5c154a344517f94511646f
maceireggre <157102399+maceireggre@users.noreply.github.com>
2024-01-24 09:18:51 +0100

    Add files via upload

---
+++ derivation_machine_learning.py
@@ -0,0 +1,12 @@
+﻿class variable:
+
+    def __valeur__(self):
+        a=a
+        b=b
+        c=a.valeur+b.valeur
+    def __derivees_locals__():
+    def __add__():
+    def __sub__():
+    def __nul__():
+        inv()
+    def __truediv__
