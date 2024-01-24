33b42cb0ee83a6619d8522ab6e5107f89f5d70b4
maceireggre <157102399+maceireggre@users.noreply.github.com>
2024-01-24 09:58:32 +0100

    Update derivation_machine_learning.py

--- derivation_machine_learning.py
+++ derivation_machine_learning.py
@@ -1,4 +1,5 @@
-﻿class variable:
+__________________
+class variable:
 
     def __valeur__(self):
         a=a
@@ -10,6 +11,37 @@
     def __nul__():
         inv()
     def __truediv__
+__________________
+class variable:
+    def __init__(self,valeur):
+        self.valeur=valeur
+    def __valeur__(self,valeur):
+        a=a
+        b=b
+        c=a.valeur+b.valeur
+    def __derivees_locals__(self,valeur):
+        return None
+
+
+
+    def __add__(self,valeur):
+        if isinstance(other, Variable):
+            [(a,1)(b,1)]
+        return Variable(self.valeur + other.valeur)
+    def __sub__(self,valeur):
+        return None
+    def __nul__(self,valeur):
+        inv()
+    def __truediv__(self,valeur):
+        if isinstance(autre, Variable):
+            if autre.valeur != 0:
+                return Variable(self.valeur / autre.valeur)
+            else:
+                raise ValueError("Division by zero")
+        else:
+            raise TypeError("Unsupported operand type for /: {}".format(type(other)))
+
+__________________
 partie chatgpt de merde
 class Variable:
     def __init__(self, valeur):
