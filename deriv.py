4fa1deeef4a0181ec5309feb4db37e3253b06a41
maceireggre <157102399+maceireggre@users.noreply.github.com>
2024-01-26 09:53:27 +0100

    Add files via upload

--- deriv.py
+++ deriv.py
@@ -1,29 +1,50 @@
-﻿class Variable:
-    def __init__(self, other):
-        self.valeur = valeur
+﻿from machine_learning import *
+
+
+class Variable:
+    def __init__(self, value,gradiant=()):
+        self.value = value
+        self.gradiant=gradiant
 
 
     def __add__(self, other):
+        addi=self.value + other.value
+        gradiant=[(self,1),(other,1)]
+        c = Variable(addi,gradiant)
+        return c
 
     def __sub__(self, other):
-        pass
+        sub =self.value - other.value
+        gradiant=[(self,1),(other,-1)]
+        d = Variable(sub,gradiant)
+        return d
 
     def __mul__(self, other):
+        mul=self.value * other.value
+        gradiant=[(self,other.value),(other,self.value)]
+        e = Variable(mul,gradiant)
+        return e
 
     def __truediv__(self, other):
+        truediv=self * other.inv()
+        return truediv
 
 
     def inv(self):
-        return str(self. other)
+        inv=1/self.value
+        gradient=[(self,(-1/(self.value)**2))]
+        f= Variable(inv,gradient)
+        return f
 
 
-a = Variable()
-b = Variable()
+a = Variable(5)
+b = Variable(6)
 
 c = a + b
-
+#print(c.gradiant)
 d = a - b
 
 e = a * b
-
+print()
 f = a / b
+print(f.value)
