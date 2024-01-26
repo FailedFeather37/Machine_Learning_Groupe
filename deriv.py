bf74741200683e0af59ab0b565b3b2b727e3c257
HarryBSSN <157102411+HarryBSSN@users.noreply.github.com>
2024-01-26 08:24:05 +0100

    Add files via upload

---
+++ deriv.py
@@ -0,0 +1,29 @@
+﻿class Variable:
+    def __init__(self, other):
+        self.valeur = valeur
+
+
+    def __add__(self, other):
+
+    def __sub__(self, other):
+        pass
+
+    def __mul__(self, other):
+
+    def __truediv__(self, other):
+
+
+    def inv(self):
+        return str(self. other)
+
+
+a = Variable()
+b = Variable()
+
+c = a + b
+
+d = a - b
+
+e = a * b
+
+f = a / b
