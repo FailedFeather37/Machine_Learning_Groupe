42768a70ff8a41ab092d22d84b47904c88dc2125
FailedFeather37 <126341708+FailedFeather37@users.noreply.github.com>
2024-02-07 09:56:28 +0100

    Add files via upload

--- class_neurone.py
+++ class_neurone.py
@@ -4,23 +4,157 @@ from math import *
 
 
 class Neurone:
-    def __init__(self):
-        self.poids1=poids1
-        self.poids2=poids2
+    def __init__(self): #1
+        self.w1=liste_poids[0]
+        self.w2=liste_poids[1]
+        self.b=b
 
-    def prediction(self): #calcul prediction selon x1 et x2 (class)
-        pass
+    def prediction(self,x1,x2): #2 calcul prediction selon x1 et x2 (class)
+        neg=Variable(-1)
+        neg=Variable(-1)
+        sig_value1=Variable(1)
+        pui=Variable(2)
+
+        self.x1_g = Variable(x1)
+        self.x2_g = Variable(x2)
+        self.w1_g = Variable (self.w1)
+        self.w2_g = Variable (self.w2)
+        self.b_g = Variable(self.b)
+
+        z=x1_g*w1_g+x2_g*w2_g + b_g
+
+        z_neg=z*neg
+        #gradient sigmoid
+        z_sig= sig_value1 / (sig_value1 + z_neg.exp())
+
+        return z_sig
+
+    def erreur(self,z_sig,i): #3 calcul de l'erreur avec les variables
+        cible_object=Variable(liste_max_cible[i])
+        #gradient erreur
+        e = (cible_object - z_sig)**pui
+        return e
+
 
     def apprentissage(self): #derive partiel avec E
-        pass
+        liste_e=[]
+        print("Estimations class")
+        for i in range(n2):
+            x1=liste_data[i]
+            x2=liste_data[i:i+1]
+            x2=x2[0]
+            self.x1_g = Variable(x1)
+            self.x2_g = Variable(x2)
+            z_sig=self.prediction(x1,x2)
+            e = self.erreur(z_sig,i)
+            # boucle pour x1 et x2
+
+            #nouvelle méthode
+            # dérivées partielles
+            de_dw1=calcul_gradient(e,w1_g)
+            de_dw2=calcul_gradient(e,w2_g)
+            de_db=calcul_gradient(e,b_g)
 
 
-def erreur(): #calcul de l'erreur avec les variables
-    pass
+            # maj des valeurs avec les gradients
+            self.w1 = self.w1-learning_rate*de_dw1
+            self.w2 = self.w2-learning_rate*de_dw2
+            self.b = self.b-learning_rate*de_db
 
+            #nouvelle méthode
+            z=x1*self.w1+x2*self.w2+b
+            z=sigmoid(z)
+
+            e = (z - liste_max_cible[i])**2
+            liste_e.append(self.b)
+
+            print(self.w1,self.w2,self.b,e)
+
+        return liste_e
+
+
+
+if __name__=="__main__":
+    neurore=Neurone()
+    a=neurore.prediction(liste_data[0],liste_data[1])
+    #b=neurore.erreur(a,2)
+    print(neurore.apprentissage())
+
+
+"""
+methode pour derive partielle et calcul de l'erreur
+"""
 """
 1. __init__ avec les poids (class)
 2. calcul prediction selon x1 et x2 (class)
 3. calcul de l'erreur avec les variables  ( dans main)
 4. apprentissage avec E (class)
+"""
+
+"""
+change valeur de x1 et x2 a chaque iteration
+boucle for = E
+1 estimation = 1 itération
+(y1^-y1)²
+(y2^-y2)²  --> E calcul gradient
+(y3^-y3)²
+(y3^-y3)²
+
+((x1,x2),y1) --> y1^
+
+1. __init__ avec les poids (class)
+2. calcul prediction selon x1 et x2 (class)
+3. calcul de l'erreur avec les variables  ( dans main)
+4. apprentissage avec E (class)
+"""
+
+
+""" prediction
+for i in range(EPOCHS):
+
+    # boucle qui pour x1 et x2
+    x1=liste_data[i]
+    x2=liste_data[i:i+1]
+    x2=x2[0]
+
+    x1_g = Variable(x1)
+    x2_g = Variable(x2)
+    w1_g = Variable (w1)
+    w2_g = Variable (w2)
+    b_g = Variable(b)
+
+    z=x1_g*w1_g+x2_g*w2_g + b_g
+
+    z_neg=z*neg
+    #gradient sigmoid
+    z_sig= sig_value1 / (sig_value1 + z_neg.exp())
+"""
+
+""" erreur
+    cible_object=Variable(liste_max_cible[i])
+    #gradient erreur
+    e = (cible_object - z_sig)**pui
+"""
+
+""" apprentissage
+    # dérivées partielles
+    de_dw1=calcul_gradient(e,w1_g)
+    de_dw2=calcul_gradient(e,w2_g)
+    de_db=calcul_gradient(e,b_g)
+    de_dx1=calcul_gradient(e,x1_g)
+    de_dx2=calcul_gradient(e,x2_g)
+
+    # maj des valeurs avec les gradients
+    w1 = w1-learning_rate*de_dw1
+    w2 = w2-learning_rate*de_dw2
+    b = b-learning_rate*de_db
+"""
+
+""" optionnel
+    #recalcul des valeurs pour listage et modélisation
+    z=x1*w1+x2*w1+b
+    z=sigmoid(z)
+
+    #recalcul valeur erreur (y1' - y1)**2
+    e = (z - liste_max_cible[i])**2
 """
--- deriv.py
+++ deriv.py
@@ -5,27 +5,27 @@ import numpy as np
 
 
 class Variable:
-    def __init__(self, value,gradiant=()):
+    def __init__(self, value,gradient=()):
         self.value = value
-        self.gradiant=gradiant
+        self.gradient=gradient
 
 
     def __add__(self, other):
         addi=self.value + other.value
-        gradiant=[(self,1),(other,1)]
-        c = Variable(addi,gradiant)
+        gradient=[(self,1),(other,1)]
+        c = Variable(addi,gradient)
         return c
 
     def __sub__(self, other):
         sub =self.value - other.value
-        gradiant=[(self,1),(other,-1)]
-        d = Variable(sub,gradiant)
+        gradient=[(self,1),(other,-1)]
+        d = Variable(sub,gradient)
         return d
 
     def __mul__(self, other):
         mul=self.value * other.value
-        gradiant=[(self,other.value),(other,self.value)]
-        e = Variable(mul,gradiant)
+        gradient=[(self,other.value),(other,self.value)]
+        e = Variable(mul,gradient)
         return e
 
     def __truediv__(self, other):
@@ -34,8 +34,8 @@ class Variable:
 
     def __pow__(self,other):
         pow=self.value ** other.value
-        gradiant=[(self,other.value*self.value**(other.value-1))]
-        g = Variable(pow,gradiant)
+        gradient=[(self,other.value*self.value**(other.value-1))]
+        g = Variable(pow,gradient)
         return g
 
     def inv(self):
@@ -46,22 +46,22 @@ class Variable:
 
     def exp(self):
         exp_value=exp(self.value)
-        gradiant=[(self,exp(self.value))]
-        h = Variable(exp_value,gradiant)
+        gradient=[(self,exp(self.value))]
+        h = Variable(exp_value,gradient)
         return h
 
 
 
 
-def calcul_gradiant(variable,rapport):
-    gradiant = 0
-    for enfant in variable.gradiant:
+def calcul_gradient(variable,rapport):
+    gradient = 0
+    for enfant in variable.gradient:
         if enfant[0] == rapport:
-            gradiant += enfant[1]
+            gradient += enfant[1]
         else:
-            recur=calcul_gradiant(enfant[0],rapport)
-            gradiant += recur * enfant[1]
-    return(gradiant)
+            recur=calcul_gradient(enfant[0],rapport)
+            gradient += recur * enfant[1]
+    return(gradient)
 
 
 
@@ -80,12 +80,12 @@ pui=Variable(2)
 
 
 for i in range(EPOCHS):
-    
+
     # boucle qui pour x1 et x2
     x1=liste_data[i]
     x2=liste_data[i:i+1]
     x2=x2[0]
-    
+
     x1_g = Variable(x1)
     x2_g = Variable(x2)
     w1_g = Variable (w1)
@@ -93,53 +93,38 @@ for i in range(EPOCHS):
     b_g = Variable(b)
 
     z=x1_g*w1_g+x2_g*w2_g + b_g
-    
+
     z_neg=z*neg
-    #gradiant sigmoid
-    z_sig= sig_value1 / (sig_value1 + z_neg.exp()) 
-    
+    #gradient sigmoid
+    z_sig= sig_value1 / (sig_value1 + z_neg.exp())
+
     cible_object=Variable(liste_max_cible[i])
-    #gradiant erreur
-    e = (cible_object - z_sig)**pui 
-    
+    #gradient erreur
+    e = (cible_object - z_sig)**pui
+
     # dérivées partielles
-    de_dw1=calcul_gradiant(e,w1_g)
-    de_dw2=calcul_gradiant(e,w2_g)
-    de_db=calcul_gradiant(e,b_g)
-    de_dx1=calcul_gradiant(e,x1_g)
-    de_dx2=calcul_gradiant(e,x2_g)
-    
-    # maj des valeurs avec les gradiants
+    de_dw1=calcul_gradient(e,w1_g)
+    de_dw2=calcul_gradient(e,w2_g)
+    de_db=calcul_gradient(e,b_g)
+    de_dx1=calcul_gradient(e,x1_g)
+    de_dx2=calcul_gradient(e,x2_g)
+
+    # maj des valeurs avec les gradients
     w1 = w1-learning_rate*de_dw1
     w2 = w2-learning_rate*de_dw2
     b = b-learning_rate*de_db
-    
-    #recalcul des valeurs pour listage et modélisation
-    z=x1*w1+x2*w1+b
-    z=sigmoid(z)
-    
-    #recalcul valeur erreur (y1' - y1)**2
-    e = (z - liste_max_cible[i])**2  
 
-    machine_learn=x1*w1+x2*w1+b
+    #recalcul des valeurs pour listage et modélisation
+    z=x1*w1+x2*w2+b
+    z=sigmoid(z)
+
+    #recalcul valeur erreur (y1' - y1)**2
+    e = (z - liste_max_cible[i])**2
     liste_learn.append(e)
-    liste_machine_learn.append(machine_learn)
-    
+
     print(w1,w2,b,e)
-    print("estimation :",machine_learn,i+1)
     #print(liste_learn)
-    
-min=liste_learn[0]
-for i in liste_learn:
-    if i<min:
-        min=i
-print(min)
-comptmin=0
-for i in liste_learn:
-    if i==min:
-        print(liste_machine_learn[comptmin])
-        break
-    comptmin+=1   
+
 
 E=0
 for i in liste_learn:
@@ -147,7 +132,7 @@ for i in liste_learn:
 #print(E)
 
 
-x_point=np.linspace(0,1,5)
+x_point=np.linspace(0,1,n2)
 y_point=liste_learn
 plt.plot(x_point,y_point)
 plt.ylabel('erreurs')
@@ -157,19 +142,3 @@ plt.show()
 
 
 
-"""
-change valeur de x1 et x2 a chaque iteration
-boucle for = E
-1 estimation = 1 itération
-(y1^-y1)²
-(y2^-y2)²  --> E calcul gradiant
-(y3^-y3)²
-(y3^-y3)²
-
-((x1,x2),y1) --> y1^
-
-1. __init__ avec les poids (class)
-2. calcul prediction selon x1 et x2 (class)
-3. calcul de l'erreur avec les variables  ( dans main)
-4. apprentissage avec E (class)
-"""
