559ee0075d8b7d19fc1e2281f489ed453cd8b36b
FailedFeather37 <126341708+FailedFeather37@users.noreply.github.com>
2024-02-15 22:43:17 +0100

    Add files via upload

--- deriv.py
+++ deriv.py
@@ -1,29 +1,28 @@
-from init_data import *
 from math import *
 
 
 class Variable:
-    def __init__(self, value,gradiant=()):
+    def __init__(self, value,gradient=()):
         self.value = value
-        self.gradiant=gradiant
+        self.gradient=gradient
 
 
     def __add__(self, other):
         addi=self.value + other.value
-        gradiant=[(self,1),(other,1)]
-        c = Variable(addi,gradiant)
+        gradient=[(self,1),(other,1)]
+        c = Variable(addi,gradient)
         return c
 
     def __sub__(self, other):
         sub =self.value - other.value
-        gradiant=[(self,1),(other,-1)]
-        d = Variable(sub,gradiant)
+        gradient=[(self,1),(other,-1)]
+        d = Variable(sub,gradient)
         return d
 
     def __mul__(self, other):
         mul=self.value * other.value
-        gradiant=[(self,other.value),(other,self.value)]
-        e = Variable(mul,gradiant)
+        gradient=[(self,other.value),(other,self.value)]
+        e = Variable(mul,gradient)
         return e
 
     def __truediv__(self, other):
@@ -32,8 +31,8 @@ class Variable:
 
     def __pow__(self,other):
         pow=self.value ** other.value
-        gradiant=[(self,other.value*self.value**(other.value-1))]
-        g = Variable(pow,gradiant)
+        gradient=[(self,other.value*self.value**(other.value-1))]
+        g = Variable(pow,gradient)
         return g
 
     def inv(self):
@@ -44,94 +43,19 @@ class Variable:
 
     def exp(self):
         exp_value=exp(self.value)
-        gradiant=[(self,exp(self.value))]
-        h = Variable(exp_value,gradiant)
+        gradient=[(self,exp(self.value))]
+        h = Variable(exp_value,gradient)
         return h
 
 
 
 
-def calcul_gradiant(variable,rapport):
-    gradiant = 0
-    for enfant in variable.gradiant:
+def calcul_gradient(variable,rapport):
+    gradient = 0
+    for enfant in variable.gradient:
         if enfant[0] == rapport:
-            gradiant += enfant[1]
+            gradient += enfant[1]
         else:
-            recur=calcul_gradiant(enfant[0],rapport)
-            gradiant += recur * enfant[1]
-    return(gradiant)
-
-
-
-
-if __name__=="__main__":
-    learning_rate=0.3
-    EPOCHS=20
-    z=f_complexe(somme)
-    
-    
-    x1=liste_data[0]
-    x2=liste_data[1]
-    w1=liste_poids[0]
-    w2=liste_poids[1]
-    b=1
-
-    x1_g = Variable(x1)
-    x2_g = Variable(x2)
-    w1_g = Variable (w1)
-    w2_g = Variable (w2)
-    b_g = Variable(b)
-
-
-    z=x1_g*w1_g+x2_g*w2_g + b_g
-    neg=Variable(-1)
-    z_neg=z*neg
-    sig_value1=Variable(1)
-    z_sig= sig_value1 / (sig_value1 + z_neg.exp()) #gradiant sigmoid
-
-    pui=Variable(2)
-    cible_object=Variable(liste_max_cible[0])
-    e = (cible_object - z_sig)**pui #gradiant erreur
-    de_dw1=calcul_gradiant(e,w1_g)
-    de_dw2=calcul_gradiant(e,w2_g)
-    de_db=calcul_gradiant(e,b_g)
-    de_dx1=calcul_gradiant(e,x1_g)
-    de_dx2=calcul_gradiant(e,x2_g)
-
-    liste_learn=[]
-    z=x1*w1+x2*w1+b
-    e = (z - liste_max_cible[0])**2
-    ecart_e=e
-    print(liste_max_cible)
-    
-    compt=0
-    for i in range(5):
-        w1 = w1-learning_rate*de_dw1
-        w2 = w2-learning_rate*de_dw2
-        b = b-learning_rate*de_db
-
-        print(w1,w2,b,e)
-        z=x1*w1+x2*w1+b
-        z=sigmoid(z)
-        
-        e = (z - liste_max_cible[i])**2 #mise à jour de l'erreur (y' - y)**2
-        
-        if e>ecart_e:
-            break
-        
-        ecart_e=e
-        liste_learn.append(e)
-        test=sigmoid(x1*w1+x2*w2+b)
-        compt+=1
-        print("estimation :",test,compt)
-        
-
-    """ calcul min des erreurs
-    min=liste_learn[0]
-    for i in liste_learn:
-        if i<min:
-            min=i
-    print(min,"min")
-    """
-    #print(w1,w2,e)
-    #print(liste_learn)
+            recur=calcul_gradient(enfant[0],rapport)
+            gradient += recur * enfant[1]
+    return(gradient)
